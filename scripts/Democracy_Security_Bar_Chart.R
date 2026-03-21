3ee7649d60e2eadc82c8b46ed7164c78c16891f4
Stephanie Lindegger <stephanie.lindegger@gmail.com>
2026-03-21 16:32:00 +0100

    Finalise This shows, how many answers are related to the topic (security) in the defined affairs (democracy) but also indicated how many affairs did not render any result that can be evaluated.

--- scripts/Democracy_Security_Bar_Chart.R
+++ scripts/Democracy_Security_Bar_Chart.R
@@ -5,7 +5,7 @@ chart_data <- readRDS("data_preprocessed/democracy_security_affairs.rds")
 #prep input: Yes, No, NA (because may affairs rendered NA)
 bar_chart_input <- chart_data %>%
   mutate(answer = replace_na(answer, "NA")) %>%
-  mutate(answer = factor(answer, levels = c("Yes", "No", "NA"))) %>%
+  mutate(answer = factor(answer, levels = c("No", "Yes", "NA"))) %>%
   count(answer)
 
 #bar chart set up
