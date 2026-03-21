373fd1867806917f79c0ef7d2a81da5f1936ea36
Stephanie Lindegger <stephanie.lindegger@gmail.com>
2026-03-21 16:20:26 +0100

    prep answers

--- scripts/Democracy_Security_Bar_Chart.R
+++ scripts/Democracy_Security_Bar_Chart.R
@@ -1,5 +1,9 @@
 
 
 
+chart_data <- readRDS("data_preprocessed/democracy_security_affairs.rds")
 
-democracy_affairs <- readRDS("data_preprocessed/democracy_security_affairs.rds")
+#prep input: Yes, No, NA (because may affairs rendered NA)
+bar_chart_input <- chart_data$classification %>%
+  mutate(answer = replace_na(answer, "NA")) %>%
+  count(answer)
